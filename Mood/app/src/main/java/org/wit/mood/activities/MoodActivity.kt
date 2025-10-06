7ef43a0f58c50377e69eec33a9c1aa260df109b5
Mdelmar05 <mmarmadridel@uma.es>
2025-10-06 17:20:46 +0100

    Added Timber logging and Snackbar validation for empty input

--- Mood/app/src/main/java/org/wit/mood/activities/MoodActivity.kt
+++ Mood/app/src/main/java/org/wit/mood/activities/MoodActivity.kt
@@ -1,22 +1,34 @@
 package org.wit.mood.activities
 
-import android.annotation.SuppressLint
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.github.ajalt.timberkt.Timber
-import org.wit.mood.R
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import org.wit.mood.databinding.ActivityMoodBinding
+import org.wit.mood.models.MoodModel
+import timber.log.Timber
 import timber.log.Timber.i
 
 class MoodActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMoodBinding
+    var mood = MoodModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mood)
+
+        binding = ActivityMoodBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
-        i("Placemark Activity started..")
+        i("Mood Activity started...")
+
+        binding.btnAdd.setOnClickListener {
+            mood.mood = binding.moodTitle.text.toString()
+            if (mood.mood.isNotEmpty()) {
+                i("Add Button Pressed: ${mood.mood}")
+            } else {
+                Snackbar.make(it, "Please enter a mood", Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 }
