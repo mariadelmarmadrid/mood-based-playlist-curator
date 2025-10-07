e558fee244533b4a88844c97ddb24b65ec01aa60
Mdelmar05 <mmarmadridel@uma.es>
2025-10-07 14:47:14 +0100

    Extra questions added"

--- Mood/app/src/main/java/org/wit/mood/activities/MoodActivity.kt
+++ Mood/app/src/main/java/org/wit/mood/activities/MoodActivity.kt
@@ -1,52 +1,64 @@
 package org.wit.mood.activities
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import org.wit.mood.databinding.ActivityMoodBinding
 import org.wit.mood.main.MainApp
-import org.wit.mood.models.MoodModel
-import org.wit.mood.models.MoodType
+import org.wit.mood.models.*
 import timber.log.Timber.i
+import java.time.LocalDateTime
+import org.wit.mood.R
 
 class MoodActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMoodBinding
-    var mood = MoodModel()
     lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMoodBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        app = application as MainApp
 
+        app = application as MainApp
         i("Mood Activity started...")
 
-        binding.btnAdd.setOnClickListener {
-            val selectedMood = when (binding.moodGroup.checkedRadioButtonId) {
-                binding.moodHappy.id -> MoodType.Happy
-                binding.moodRelaxed.id -> MoodType.Relaxed
-                binding.moodNeutral.id -> MoodType.Neutral
-                binding.moodSad.id -> MoodType.Sad
-                binding.moodAngry.id -> MoodType.Angry
-                else -> null
-            }
+        // Setup spinners
+        setupSpinner(binding.spinnerMoodType, R.array.mood_types)
+        setupSpinner(binding.spinnerSleep, R.array.sleep_quality)
+        setupSpinner(binding.spinnerSocial, R.array.social_activity)
+        setupSpinner(binding.spinnerHobby, R.array.hobbies)
+        setupSpinner(binding.spinnerFood, R.array.food_types)
 
-            if (selectedMood != null) {
-                mood.type = selectedMood
-                mood.note = binding.note.text.toString()
-                app.moods.add(mood.copy())
-                i("Mood Added: $mood")
-                for (i in app.moods.indices) {
-                    i("Mood[$i]: ${app.moods[i]}")
-                }
+        binding.btnAddMood.setOnClickListener {
+            val mood = MoodModel(
+                type = MoodType.valueOf(binding.spinnerMoodType.selectedItem.toString().uppercase()),
+                note = binding.editNote.text.toString(),
+                sleep = SleepQuality.valueOf(binding.spinnerSleep.selectedItem.toString().uppercase()),
+                social = SocialActivity.valueOf(binding.spinnerSocial.selectedItem.toString().uppercase()),
+                hobby = Hobby.valueOf(binding.spinnerHobby.selectedItem.toString().uppercase()),
+                food = FoodType.valueOf(binding.spinnerFood.selectedItem.toString().uppercase()),
+                timestamp = LocalDateTime.now().toString()
+            )
 
-                setResult(RESULT_OK)
-                finish()
-            } else {
-                Snackbar.make(it, "Please select a mood", Snackbar.LENGTH_LONG).show()
-            }
+            app.moods.add(mood)
+            i("Mood Added: $mood")
+
+            Snackbar.make(binding.root, "Mood Added!", Snackbar.LENGTH_SHORT).show()
+            setResult(RESULT_OK)
+            finish()
+        }
+    }
+
+    private fun setupSpinner(spinner: android.widget.Spinner, arrayRes: Int) {
+        ArrayAdapter.createFromResource(
+            this,
+            arrayRes,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
         }
     }
 }
--- Mood/app/src/main/java/org/wit/mood/models/MoodEnums.kt
+++ Mood/app/src/main/java/org/wit/mood/models/MoodEnums.kt
@@ -1,6 +1,13 @@
 package org.wit.mood.models
 
-enum class MoodType { HAPPY, RELAXED, NEUTRAL, SAD, ANGRY }
+enum class MoodType(val label: String, val score: Int) {
+    Happy("Happy 😊", 2),
+    Relaxed("Relaxed 😌", 1),
+    Neutral("Neutral 😐", 0),
+    Sad("Sad 😢", -1),
+    Angry("Angry 😠", -2);
+}
+
 enum class SleepQuality { GOOD, MEDIUM, POOR }
 enum class SocialActivity { FAMILY, FRIENDS, DATE, PARTY, NONE }
 enum class Hobby { MOVIES, READING, GAMES, SPORT, RELAXATION, NONE }
