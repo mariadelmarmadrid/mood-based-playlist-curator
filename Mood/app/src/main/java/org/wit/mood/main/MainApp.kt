96fee66d2aa39c662f3ed6b3a186df4efe47e81f
Mdelmar05 <mmarmadridel@uma.es>
2025-10-17 19:04:34 +0100

    Point my MainApp at the interface MoodStore, not the class MoodJSONStore

--- Mood/app/src/main/java/org/wit/mood/main/MainApp.kt
+++ Mood/app/src/main/java/org/wit/mood/main/MainApp.kt
@@ -2,13 +2,13 @@ package org.wit.mood.main
 
 import android.app.Application
 import org.wit.mood.models.MoodJSONStore
-import org.wit.mood.models.MoodModel
+import org.wit.mood.models.MoodStore
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MainApp : Application() {
 
-    lateinit var moods: MoodJSONStore
+    lateinit var moods: MoodStore
 
     override fun onCreate() {
         super.onCreate()
